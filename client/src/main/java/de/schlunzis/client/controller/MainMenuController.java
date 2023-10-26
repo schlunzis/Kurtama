66aec433270314e7a2cbc740a70179d48a06a294
Tilman Holube <tilman@holube.de>
2023-10-26 17:08:54 +0200

    main menu corrections

--- client/src/main/java/de/schlunzis/client/controller/MainMenuController.java
+++ client/src/main/java/de/schlunzis/client/controller/MainMenuController.java
@@ -1,7 +1,9 @@
 package de.schlunzis.client.controller;
 
 import com.google.common.eventbus.EventBus;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,9 @@ import org.springframework.stereotype.Component;
 public class MainMenuController {
 
     private final EventBus eventBus;
+
+    @FXML
+    private ListView lobbiesListView; // TODO implement
 
     public MainMenuController(EventBus eventBus) {
         this.eventBus = eventBus;
@@ -20,6 +25,21 @@ public class MainMenuController {
     private void logout() {
         log.info("Logout button clicked");
         // TODO implement, see issue
+    }
+
+    @FXML
+    private void settings(ActionEvent actionEvent) {
+        // TODO implement
+    }
+
+    @FXML
+    private void joinLobby(ActionEvent actionEvent) {
+        // TODO implement
+    }
+
+    @FXML
+    private void createLobby(ActionEvent actionEvent) {
+        // TODO implement
     }
 
 }
