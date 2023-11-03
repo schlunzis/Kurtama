864387c216ba17f3c74d8ef22dc4831527371283
Tilman Holube <tilman@holube.de>
2023-11-03 16:31:48 +0100

    client dialog for lobby creation

--- client/src/main/java/org/schlunzis/kurtama/client/fx/controller/MainMenuController.java
+++ client/src/main/java/org/schlunzis/kurtama/client/fx/controller/MainMenuController.java
@@ -2,13 +2,19 @@ package org.schlunzis.kurtama.client.fx.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.ListView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.schlunzis.kurtama.client.fx.dialog.CreateLobbyDialogResult;
+import org.schlunzis.kurtama.client.fx.dialog.DialogFactory;
 import org.schlunzis.kurtama.common.messages.authentication.logout.LogoutRequest;
+import org.schlunzis.kurtama.common.messages.lobby.CreateLobbyRequest;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Slf4j
 @FxmlView("main.fxml")
@@ -17,6 +23,7 @@ import org.springframework.stereotype.Component;
 public class MainMenuController {
 
     private final ApplicationEventPublisher eventBus;
+    private final DialogFactory dialogFactory;
 
     @FXML
     private ListView lobbiesListView; // TODO implement
@@ -40,7 +47,10 @@ public class MainMenuController {
 
     @FXML
     private void createLobby(ActionEvent actionEvent) {
-        // TODO implement
+        Optional<Dialog<CreateLobbyDialogResult>> dialog = dialogFactory.createCreateLobbyDialog();
+        dialog.flatMap(Dialog::showAndWait).ifPresent(result ->
+                eventBus.publishEvent(new CreateLobbyRequest(result.getName()))
+        );
     }
 
 }
