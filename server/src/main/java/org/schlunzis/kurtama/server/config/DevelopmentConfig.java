f46fc8d2532655946742c23531e52bdd821a34f6
Jonas Pohl <jonas.jp4c@gmail.com>
2023-11-03 14:50:17 +0100

    Server profile

---
+++ server/src/main/java/org/schlunzis/kurtama/server/config/DevelopmentConfig.java
@@ -0,0 +1,35 @@
+package org.schlunzis.kurtama.server.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.schlunzis.kurtama.server.user.IUserStore;
+import org.schlunzis.kurtama.server.user.ServerUser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.UUID;
+
+/**
+ * Configuration class for development environment.
+ * <p>
+ * All configurations will only be applied if the profile "dev" is active.
+ */
+@Slf4j
+@Configuration
+@Profile("dev")
+@RequiredArgsConstructor
+public class DevelopmentConfig {
+
+    private final IUserStore userStore;
+
+    @Bean
+    void developmentInitialization() {
+        // creating some test users
+        for (int i = 0; i < 10; i++) {
+            UUID id = userStore.createUser(new ServerUser("test" + i + "@test.de", "test" + i, "test" + i));
+            log.debug("Created user with id {}", id);
+        }
+    }
+
+}
--- server/src/main/java/org/schlunzis/kurtama/server/user/H2UserStore.java
+++ server/src/main/java/org/schlunzis/kurtama/server/user/H2UserStore.java
@@ -1,12 +1,14 @@
 package org.schlunzis.kurtama.server.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Profile("!production")
 @RequiredArgsConstructor
 public class H2UserStore implements IUserStore {
 
