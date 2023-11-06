abffdd80bebdfe7d55e665c5d02d44bafff48a96
Jonas Pohl <jonas.jp4c@gmail.com>
2023-11-06 23:44:55 +0100

    Fixing typo in RegisterSuccessfulResponse

--- common/src/main/java/org/schlunzis/kurtama/common/messages/authentication/register/RegisterSuccessfulResponse.java
+++ common/src/main/java/org/schlunzis/kurtama/common/messages/authentication/register/RegisterSuccessfulResponse.java
@@ -2,5 +2,5 @@ package org.schlunzis.kurtama.common.messages.authentication.register;
 
 import org.schlunzis.kurtama.common.messages.IServerMessage;
 
-public class RegisterSuccessfullResponse implements IServerMessage {
+public class RegisterSuccessfulResponse implements IServerMessage {
 }
