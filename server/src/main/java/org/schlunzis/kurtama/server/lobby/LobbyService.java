31c9beba33f752e361a6974938f613fbf346b8e6
Tamlin <tilman@holube.de>
2023-11-24 22:26:12 +0100

    post merge conflict clean up

--- server/src/main/java/org/schlunzis/kurtama/server/lobby/LobbyService.java
+++ server/src/main/java/org/schlunzis/kurtama/server/lobby/LobbyService.java
@@ -5,11 +5,9 @@ import org.schlunzis.kurtama.common.LobbyInfo;
 import org.schlunzis.kurtama.common.messages.lobby.client.CreateLobbyRequest;
 import org.schlunzis.kurtama.common.messages.lobby.client.JoinLobbyRequest;
 import org.schlunzis.kurtama.common.messages.lobby.client.LeaveLobbyRequest;
-import org.schlunzis.kurtama.common.messages.lobby.server.JoinLobbySuccessfullyResponse;
-import org.schlunzis.kurtama.common.messages.lobby.server.LeaveLobbySuccessfullyResponse;
-import org.schlunzis.kurtama.common.messages.lobby.server.LobbyCreatedSuccessfullyResponse;
-import org.schlunzis.kurtama.common.messages.lobby.server.LobbyListInfoMessage;
+import org.schlunzis.kurtama.common.messages.lobby.server.*;
 import org.schlunzis.kurtama.server.auth.AuthenticationService;
+import org.schlunzis.kurtama.server.lobby.exception.LobbyNotFoundException;
 import org.schlunzis.kurtama.server.net.ClientMessageWrapper;
 import org.schlunzis.kurtama.server.service.AbstractService;
 import org.springframework.context.ApplicationEventPublisher;
@@ -32,42 +30,49 @@ public class LobbyService extends AbstractService {
     @EventListener
     public void onCreateLobbyRequest(ClientMessageWrapper<CreateLobbyRequest> cmw) {
         CreateLobbyRequest request = cmw.clientMessage();
-
-        ServerLobby lobby = lobbyManagement.createLobby(request.name(), user.get());
-        sendTo(new LobbyCreatedSuccessfullyResponse(lobby.toDTO()), cmw.user());
-        updateLobbyListInfo();
-
-        // TODO exceptions?
-        //log.info("Could not create lobby. No user found for session.");
-        //sendTo(new LobbyCreationFailedResponse(), cmw.session());
+        try {
+            ServerLobby lobby = lobbyManagement.createLobby(request.name(), cmw.user());
+            sendTo(new LobbyCreatedSuccessfullyResponse(lobby.toDTO()), cmw.user());
+            updateLobbyListInfo();
+        } catch (Exception e) {
+            log.info("Could not create lobby. No user found for session.");
+            sendTo(new LobbyCreationFailedResponse(), cmw.session());
+        }
     }
 
     @EventListener
     public void onJoinLobbyRequest(ClientMessageWrapper<JoinLobbyRequest> cmw) {
         JoinLobbyRequest request = cmw.clientMessage();
 
-        ServerLobby lobby = lobbyManagement.joinLobby(request.lobbyID(), user.get());
-        sendTo(new JoinLobbySuccessfullyResponse(lobby.toDTO()), cmw.user());
-        // TODO update clients of users in lobby #8
-        updateLobbyListInfo();
-
-        // TODO exceptions?
-        //log.info("Could not join lobby. No user found for session.");
-        //sendTo(new JoinLobbyFailedResponse(), cmw.session());
+        try {
+            ServerLobby lobby = lobbyManagement.joinLobby(request.lobbyID(), cmw.user());
+            sendTo(new JoinLobbySuccessfullyResponse(lobby.toDTO()), cmw.user());
+            // TODO update clients of users in lobby #8
+            updateLobbyListInfo();
+        } catch (LobbyNotFoundException e) {
+            log.info("Could not join lobby. Lobby not found.");
+            sendTo(new JoinLobbyFailedResponse(), cmw.session());
+        } catch (Exception e) {
+            log.error("Could not join lobby.", e);
+            sendTo(new JoinLobbyFailedResponse(), cmw.session());
+        }
     }
 
     @EventListener
     public void onLeaveLobbyRequest(ClientMessageWrapper<LeaveLobbyRequest> cmw) {
         LeaveLobbyRequest request = cmw.clientMessage();
-
-        lobbyManagement.leaveLobby(request.lobbyID(), user.get());
-        sendTo(new LeaveLobbySuccessfullyResponse(), cmw.user());
-        // TODO update clients of users in lobby #8
-        updateLobbyListInfo();
-
-        //TODO exceptions?
-        //log.info("Could not leave lobby. No user found for session.");
-        //sendTo(new LobbyCreationFailedResponse(), cmw.session());
+        try {
+            lobbyManagement.leaveLobby(request.lobbyID(), cmw.user());
+            sendTo(new LeaveLobbySuccessfullyResponse(), cmw.user());
+            // TODO update clients of users in lobby #8
+            updateLobbyListInfo();
+        } catch (LobbyNotFoundException e) {
+            log.info("Could not leave lobby. Lobby not found.");
+            sendTo(new LobbyLeaveFailedResponse(), cmw.user());
+        } catch (Exception e) {
+            log.error("Could not leave lobby.", e);
+            sendTo(new LobbyLeaveFailedResponse(), cmw.user());
+        }
     }
 
     private void updateLobbyListInfo() {
