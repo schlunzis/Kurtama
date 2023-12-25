7cb16bfe292c6f9e1299efbb4507a0773ff0981f
Tilman Holube <tilman@holube.de>
2023-12-25 13:15:56 +0100

    make UserSessionMap and ChatManagement thread safe

--- server/src/main/java/org/schlunzis/kurtama/server/auth/UserSessionMap.java
+++ server/src/main/java/org/schlunzis/kurtama/server/auth/UserSessionMap.java
@@ -5,54 +5,99 @@ import org.schlunzis.kurtama.server.user.ServerUser;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Component
 class UserSessionMap {
 
     private final Map<ServerUser, ISession> map = new HashMap<>();
 
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
+    private final ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
+
     public void put(ServerUser user, ISession session) {
         Objects.requireNonNull(user);
         Objects.requireNonNull(session);
-        map.put(user, session);
+        writeLock.lock();
+        try {
+            map.put(user, session);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public Optional<ISession> get(ServerUser user) {
         Objects.requireNonNull(user);
-        return Optional.ofNullable(map.get(user));
+        readLock.lock();
+        try {
+            return Optional.ofNullable(map.get(user));
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public Optional<ServerUser> get(ISession session) {
         Objects.requireNonNull(session);
-        Optional<Map.Entry<ServerUser, ISession>> optionalEntry = map.entrySet().stream().filter(e -> e.getValue().equals(session)).findFirst();
-        return optionalEntry.map(Map.Entry::getKey);
+        readLock.lock();
+        try {
+            Optional<Map.Entry<ServerUser, ISession>> optionalEntry = map.entrySet().stream().filter(e -> e.getValue().equals(session)).findFirst();
+            return optionalEntry.map(Map.Entry::getKey);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public Collection<ISession> getFor(Collection<ServerUser> users) {
         Objects.requireNonNull(users);
-        return users.stream()
-                .map(map::get)
-                .filter(Objects::nonNull)
-                .toList();
+        readLock.lock();
+        try {
+            return users.stream()
+                    .map(map::get)
+                    .filter(Objects::nonNull)
+                    .toList();
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public Collection<ISession> getAllSessions() {
-        return map.values();
+        readLock.lock();
+        try {
+            return map.values();
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public boolean contains(ISession session) {
         Objects.requireNonNull(session);
-        return map.containsValue(session);
+        readLock.lock();
+        try {
+            return map.containsValue(session);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void remove(ServerUser user) {
         Objects.requireNonNull(user);
-        map.remove(user);
+        writeLock.lock();
+        try {
+            map.remove(user);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void remove(ISession session) {
         Objects.requireNonNull(session);
-        map.entrySet().removeIf(entry -> entry.getValue().equals(session));
+        writeLock.lock();
+        try {
+            map.entrySet().removeIf(entry -> entry.getValue().equals(session));
+        } finally {
+            writeLock.unlock();
+        }
     }
 
 }
