a74dbc65a342ad48d245fe05fe423df3dc4316fb
Tilman Holube <tilman@holube.de>
2024-02-25 19:21:07 +0100

    no argument feedback

--- build-client.sh
+++ build-client.sh
@@ -29,9 +29,7 @@ if  [ "$1" = linux ]; then
   --linux-app-category Game \
   --linux-app-release ${version} \
   --linux-shortcut
-fi
-
-if  [ "$1" = windows ]; then
+elif  [ "$1" = windows ]; then
   echo "Building for Windows"
   echo "Building client jar"
   ./mvn.cmd --projects client --also-make --batch-mode --update-snapshots clean install package
@@ -39,14 +37,16 @@ if  [ "$1" = windows ]; then
   echo "Running jpackage"
   jpackage --type exe \
   --input ${input} \
-    --main-jar ${main-jar} \
-    --resource-dir ${resource-dir} \
-    --name ${name} \
-    --app-version ${version} \
-    --main-class ${main-class} \
-    --dest ${destination} \
-    --win-upgrade-uuid "54e9b129-e6a4-4272-bd94-13079eb6ae6d" \
-    --win-menu
+  --main-jar ${main-jar} \
+  --resource-dir ${resource-dir} \
+  --name ${name} \
+  --app-version ${version} \
+  --main-class ${main-class} \
+  --dest ${destination} \
+  --win-upgrade-uuid "54e9b129-e6a4-4272-bd94-13079eb6ae6d" \
+  --win-menu
+else
+  echo "No OS given. Provide 'linux' or 'windows' as an argument."
 fi
 
 exit 0
