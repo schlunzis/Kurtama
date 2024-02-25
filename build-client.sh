3a9e7a5bfd12a0afb6b309cd40a84c16b1d9960c
Tilman Holube <tilman@holube.de>
2024-02-25 21:37:39 +0100

    changes

--- build-client.sh
+++ build-client.sh
@@ -32,8 +32,9 @@ if  [ "$1" = linux ]; then
 elif  [ "$1" = windows ]; then
   echo "Building for Windows"
   echo "Building client jar"
-  cmd.exe /c ./mvn.cmd --projects client --also-make --batch-mode --update-snapshots clean install package
+  echo "$(cmd.exe /c ./mvn.cmd --projects client --also-make --batch-mode --update-snapshots clean install package)"
   wait
+  ls -al ./client/target
 
   echo "Running jpackage"
   jpackage --type exe \
