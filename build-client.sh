dfa52b12aa8698629882e4815ddf96c40ef64616
Tilman Holube <tilman@holube.de>
2024-02-25 19:05:43 +0100

    build script for windows and linux

--- build-client.sh
+++ build-client.sh
@@ -3,24 +3,50 @@
 set -e
 
 version="0.0.1-SNAPSHOT"
+input="./client/target"
+main-jar="./client/target/client-${version}.jar"
+resource-dir="./client/jpackage"
+main-class="ClientLauncher"
+destination="./target/jpackage-out"
+name="kurtama-client"
 
-echo "Building client jar"
-./mvnw --projects client --also-make --batch-mode --update-snapshots clean install package
+if  [ "$1" = linux ]; then
+  echo "Building for Linux"
+  echo "Building client jar"
+  ./mvnw --projects client --also-make --batch-mode --update-snapshots clean install package
 
-echo "Running jpackage"
-jpackage --type deb \
---input ./client/target \
---main-jar ./client/target/client-${version}.jar \
---resource-dir ./client/jpackage \
---name kurtama-client \
---app-version ${version} \
---linux-package-name kurtama-client \
---linux-menu-group Game \
---linux-app-category Game \
---linux-app-release ${version} \
---linux-shortcut \
---main-class ClientLauncher \
---verbose \
---dest ./target/jpackage-out
+  echo "Running jpackage"
+  jpackage --type deb \
+  --input ${input} \
+  --main-jar ${main-jar} \
+  --resource-dir ${resource-dir} \
+  --name ${name} \
+  --app-version ${version} \
+  --main-class ${main-class} \
+  --dest ${destination} \
+  --linux-package-name kurtama-client \
+  --linux-menu-group Game \
+  --linux-app-category Game \
+  --linux-app-release ${version} \
+  --linux-shortcut
+fi
+
+if  [ "$1" = windows ]; then
+  echo "Building for Windows"
+  echo "Building client jar"
+  ./mvn.cmd --projects client --also-make --batch-mode --update-snapshots clean install package
+
+  echo "Running jpackage"
+  jpackage --type exe \
+  --input ${input} \
+    --main-jar ${main-jar} \
+    --resource-dir ${resource-dir} \
+    --name ${name} \
+    --app-version ${version} \
+    --main-class ${main-class} \
+    --dest ${destination} \
+    --win-upgrade-uuid "54e9b129-e6a4-4272-bd94-13079eb6ae6d" \
+    --win-menu
+fi
 
 exit 0
