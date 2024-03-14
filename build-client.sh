ec38fbe3371151a0877d28ec54e2acab0146a457
Tilman Holube <45685205+Til7701@users.noreply.github.com>
2024-03-14 17:29:31 +0100

    121 issue packaging (#155)
    * automatic dev build with github actions
    
    * correcting file name
    
    * no inputs in job name?
    
    * inputs not input
    
    * release correction
    
    * mvnw command correction for windows
    
    * missing {
    
    * jar file modifier
    
    * tag as release name
    
    * file name modifier for windows
    
    * module specific workflows
    
    * name correction
    
    * rename some stuff
    
    * added tagged build
    TODO call it on tags starting with v and deconstruct that tag
    
    * tagged build with version substring
    
    * uniform maven options
    
    * build with jpacakge to deb
    
    * build in separate file
    
    * name correction
    
    * build script for windows and linux
    
    * no argument feedback
    
    * name corrections
    
    * jar path correction, windows correct script call
    
    * correct windows call
    
    * rename
    
    * ls
    
    * windows is bad
    
    * duplicate version
    
    * changes
    
    * move version definition to action
    
    * changes
    
    * clean up
    
    * correct class name
    
    * remove main class declaration
    
    * bash script on windows maybe
    
    * update workflow version
    
    * correcting version substring in tagged build
    
    * automatically create windows version string
    
    * more explicit workflow runs on path pushes
    
    * update readme
    
    * remove redundant name
    
    * ignore error while remove non-existent files
    
    * update dev build paths
    
    * introducing debug build
    
    * replace name with linux package name
    
    * tag as release name
    
    * correcting .desktop file name
    
    * clean up
    
    * clean up fix
    
    * rename workflow
    
    * file name correction
    
    * testing different release action
    
    * updating names and tag
    
    * correcting path
    
    * Revert "correcting path"
    
    This reverts commit 55d4720ce3e3fedad173afdf564e5c970bd61148.
    
    * Revert "updating names and tag"
    
    This reverts commit cd7bc178bd9ccfe13a33d3b496d0f6df95fafe3c.
    
    * Revert "testing different release action"
    
    This reverts commit 07ed7d465b5a83a784aedb8eb043b80dbd19eb62.
    
    * version default
    
    * version default
    
    * moving input folder to package only necessary artefacts
    
    * moving input folder to package only necessary artefacts
    
    * move release to separate action
    
    * updating link to binary
    
    * update debug build path for jpackage resource dir
    
    * update preferences path
    
    * updated with automatic alpha versioning
    
    * on push
    
    * need
    
    * ncipollo release action and not SNAPSHOT
    
    * yaml to yml
    
    * neeeeeeeeed
    
    * release body
    
    * entry point definition

---
+++ build-client.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+
+os="$1" #TODO auto detect
+version="$2"
+windowsVersion="$3"
+input="./client/target/jpackage-in"
+mainJar="client-${version}.jar"
+resourceDir="./client/jpackage"
+destination="./target/jpackage-out"
+name="Kurtama"
+
+if  [ "$os" = linux ]; then
+  echo "Building for Linux"
+  echo "Building client jar"
+  ./mvnw --projects client --also-make --batch-mode --update-snapshots clean install package
+  mkdir ${input}
+  cp client/target/${mainJar} ${input}
+
+  echo "Running jpackage"
+  jpackage --type deb \
+  --verbose \
+  --input ${input} \
+  --main-jar "${mainJar}" \
+  --resource-dir ${resourceDir} \
+  --name ${name} \
+  --app-version "${version}" \
+  --dest ${destination} \
+  --linux-package-name "kurtama-client" \
+  --linux-menu-group Game \
+  --linux-app-category Game \
+  --linux-shortcut
+
+
+elif  [ "$os" = windows ]; then
+  echo "Building for Windows"
+  echo "Building client jar"
+  # maven wrapper is a hassle here maybe this works
+  ./mvnw --projects client --also-make --batch-mode --update-snapshots clean install package
+  mkdir ${input}
+  cp client/target/${mainJar} ${input}
+
+  echo "Running jpackage"
+  jpackage --type exe \
+  --verbose \
+  --input ${input} \
+  --main-jar "${mainJar}" \
+  --resource-dir ${resourceDir} \
+  --name ${name} \
+  --app-version "${windowsVersion}" \
+  --dest ${destination} \
+  --win-upgrade-uuid "54e9b129-e6a4-4272-bd94-13079eb6ae6d" \
+  --win-menu
+
+
+else
+  echo "No OS given. Provide 'linux' or 'windows' as an argument."
+fi
+
+ls -al ${destination}
+
+exit 0
