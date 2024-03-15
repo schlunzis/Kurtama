e6d004663b19b399499d3afabdcc9cd4b7abaf21
Tilman Holube <45685205+Til7701@users.noreply.github.com>
2024-03-15 16:26:37 +0100

    85 feature request scroll chat down automatically unless paused (#161)
    * wip
    
    * automatically scrolling down unless paused
    
    * Update client/src/main/java/org/schlunzis/kurtama/client/fx/controller/ChatController.java
    
    Co-authored-by: Jonas Pohl <jonas.jp4c@gmail.com>
    
    * Update client/src/main/java/org/schlunzis/kurtama/client/fx/controller/ChatController.java
    
    Co-authored-by: Jonas Pohl <jonas.jp4c@gmail.com>
    
    * listening to scroll bar instead of on scroll event
    
    ---------
    
    Co-authored-by: Jonas Pohl <jonas.jp4c@gmail.com>

--- client/src/main/java/org/schlunzis/kurtama/client/fx/controller/ChatController.java
+++ client/src/main/java/org/schlunzis/kurtama/client/fx/controller/ChatController.java
@@ -1,9 +1,10 @@
 package org.schlunzis.kurtama.client.fx.controller;
 
+import javafx.application.Platform;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.rgielen.fxweaver.core.FxmlView;
@@ -19,39 +20,87 @@ public class ChatController {
     private final IChatService chatService;
 
     @FXML
+    private AnchorPane root;
+    @FXML
     private ListView<String> chatListView;
     @FXML
     private TextField chatTextField;
     @FXML
     private TextField senderNameTextField;
+    @FXML
+    private Button scrollDownButton;
+
+    private volatile boolean paused = false;
+    private ScrollBar verticalBar;
 
     @FXML
     public void initialize() {
         chatListView.setItems(chatService.getChatMessages());
         senderNameTextField.setText(chatService.getCurrentUsername());
-        chatListView.setCellFactory(listView -> new ListCell<>() {
-            @Override
-            protected void updateItem(String message, boolean empty) {
-                super.updateItem(message, empty);
-                if (empty || message == null) {
-                    setText(null);
-                } else {
-                    setText(message);
-                }
-                this.setFocusTraversable(false);
-                this.setMouseTransparent(true);
-                this.setDisable(true);
-            }
-        });
+        chatListView.setCellFactory(listView -> new CustomListCell());
+        chatService.getChatMessages().addListener((ListChangeListener<String>) change -> onNewChatMessage());
+        root.widthProperty().addListener((observable, oldValue, newValue) -> updateScrollDownButtonPosition());
+        scrollDownButton.widthProperty().addListener((observable, oldValue, newValue) -> updateScrollDownButtonPosition());
+        chatTextField.heightProperty().addListener((observable, oldValue, newValue) -> updateScrollDownButtonPosition());
+        setPaused(false);
     }
 
     @FXML
     private void sendMessage() {
-        String text = chatTextField.getText();
+        String text = chatTextField.getText().strip();
+        String senderName = senderNameTextField.getText().strip();
         if (text.isBlank())
             return;
-        chatService.sendMessage(senderNameTextField.getText(), text);
+        chatService.sendMessage(senderName, text);
         chatTextField.setText("");
+    }
+
+    @FXML
+    private void onScrollDownButton() {
+        scrollDown();
+        setPaused(false);
+    }
+
+    private void onNewChatMessage() {
+        if (verticalBar == null) {
+            verticalBar = (ScrollBar) chatListView.lookup(".scroll-bar:vertical");
+            verticalBar.valueProperty().addListener((obs, oldValue, newValue) ->
+                    setPaused(newValue.doubleValue() < verticalBar.getMax())
+            );
+        }
+        if (paused) return;
+        Platform.runLater(this::scrollDown);
+    }
+
+    private void scrollDown() {
+        chatListView.scrollTo(chatService.getChatMessages().size() - 1);
+    }
+
+    private void updateScrollDownButtonPosition() {
+        double newButtonPositionLeft = (root.getWidth() / 2.0) - (scrollDownButton.getWidth() / 2.0);
+        double newButtonPositionTop = chatTextField.getHeight() + 10.0;
+        AnchorPane.setLeftAnchor(scrollDownButton, newButtonPositionLeft);
+        AnchorPane.setBottomAnchor(scrollDownButton, newButtonPositionTop);
+    }
+
+    private void setPaused(boolean paused) {
+        scrollDownButton.setVisible(paused);
+        this.paused = paused;
+    }
+
+    private static class CustomListCell extends ListCell<String> {
+        @Override
+        protected void updateItem(String message, boolean empty) {
+            super.updateItem(message, empty);
+            if (empty || message == null) {
+                setText(null);
+            } else {
+                setText(message);
+            }
+            this.setFocusTraversable(false);
+            this.setMouseTransparent(true);
+            this.setDisable(true);
+        }
     }
 
 }
