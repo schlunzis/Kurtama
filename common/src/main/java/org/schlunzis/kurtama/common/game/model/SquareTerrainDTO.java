630b635b56b3dd5acb53dd7befde8eabe0621cb6
Tilman Holube <tilman@holube.de>
2024-03-22 15:29:36 +0100

    remove equals and hashcode and toString in records

--- common/src/main/java/org/schlunzis/kurtama/common/game/model/SquareTerrainDTO.java
+++ common/src/main/java/org/schlunzis/kurtama/common/game/model/SquareTerrainDTO.java
@@ -1,38 +1,10 @@
 package org.schlunzis.kurtama.common.game.model;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public record SquareTerrainDTO(int width,
                                int height,
                                SquareTileDTO[][] tiles,
                                List<EdgeDTO> edges
 ) implements ITerrainDTO {
-
-    @Override
-    public String toString() {
-        return "SquareTerrainDTO{" +
-                "width=" + width +
-                ", height=" + height +
-                ", tiles=" + Arrays.toString(tiles) +
-                ", edges=" + edges +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SquareTerrainDTO that = (SquareTerrainDTO) o;
-        return width == that.width && height == that.height && Arrays.deepEquals(tiles, that.tiles);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(width, height);
-        result = 31 * result + Arrays.deepHashCode(tiles);
-        return result;
-    }
-
 }
--- common/src/main/java/org/schlunzis/kurtama/common/game/model/SquareTileDTO.java
+++ common/src/main/java/org/schlunzis/kurtama/common/game/model/SquareTileDTO.java
@@ -2,32 +2,7 @@ package org.schlunzis.kurtama.common.game.model;
 
 import org.schlunzis.kurtama.common.IUser;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public record SquareTileDTO(int id, int[] neighbours, List<IUser> figures) implements ITileDTO {
-
-    @Override
-    public String toString() {
-        return "SquareTileDTO{" +
-                "id=" + id +
-                ", neighbours=" + Arrays.toString(neighbours) +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SquareTileDTO that = (SquareTileDTO) o;
-        return id == that.id && Arrays.equals(neighbours, that.neighbours);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(id);
-        result = 31 * result + Arrays.hashCode(neighbours);
-        return result;
-    }
 }
