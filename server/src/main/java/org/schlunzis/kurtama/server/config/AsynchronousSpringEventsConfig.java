d6b896d374871eb85bf99fdb7490bebd1c565c4a
Jonas Pohl <jonas.jp4c@gmail.com>
2024-06-12 15:55:48 +0200

    Use named virtual threads

--- server/src/main/java/org/schlunzis/kurtama/server/config/AsynchronousSpringEventsConfig.java
+++ server/src/main/java/org/schlunzis/kurtama/server/config/AsynchronousSpringEventsConfig.java
@@ -4,18 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
-
-import java.util.concurrent.Executors;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
 
 @Configuration
 public class AsynchronousSpringEventsConfig {
 
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
-        SimpleApplicationEventMulticaster eventMulticaster =
-                new SimpleApplicationEventMulticaster();
-
-        eventMulticaster.setTaskExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new VirtualThreadTaskExecutor("vt-"));
         return eventMulticaster;
     }
 
