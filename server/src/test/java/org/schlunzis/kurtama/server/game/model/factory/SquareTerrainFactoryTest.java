7e281ece89d64a2e557e8678ad374f34314f4afa
Tilman Holube <tilman@holube.de>
2024-07-26 11:01:46 +0200

    fix compilation error in test

--- server/src/test/java/org/schlunzis/kurtama/server/game/model/factory/SquareTerrainFactoryTest.java
+++ server/src/test/java/org/schlunzis/kurtama/server/game/model/factory/SquareTerrainFactoryTest.java
@@ -3,7 +3,8 @@ package org.schlunzis.kurtama.server.game.model.factory;
 import org.junit.jupiter.api.Test;
 import org.schlunzis.kurtama.common.game.GameSettings;
 import org.schlunzis.kurtama.server.game.model.SquareTerrain;
-import org.schlunzis.kurtama.server.game.model.factory.SquareTerrainFactory;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +12,7 @@ class SquareTerrainFactoryTest {
 
     @Test
     void createTerrainIndexTest() {
-        GameSettings gameSettings = new GameSettings(2, 3);
+        GameSettings gameSettings = new GameSettings(2, 3, new ArrayList<>());
         SquareTerrainFactory squareTerrainFactory = new SquareTerrainFactory(gameSettings);
         SquareTerrain terrain = (SquareTerrain) squareTerrainFactory.create();
         for (int i = 0; i < 6; i++) {
