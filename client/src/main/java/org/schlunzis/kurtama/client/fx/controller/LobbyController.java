042bd2da2334c2b19268271a53e50e496e2224c9
Jonas Pohl <jonas.jp4c@gmail.com>
2024-08-05 16:15:07 +0200

    Make start game button invisible if user is not owner

--- client/src/main/java/org/schlunzis/kurtama/client/fx/controller/LobbyController.java
+++ client/src/main/java/org/schlunzis/kurtama/client/fx/controller/LobbyController.java
@@ -1,11 +1,13 @@
 package org.schlunzis.kurtama.client.fx.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.schlunzis.kurtama.client.service.ILobbyService;
+import org.schlunzis.kurtama.client.service.impl.SessionService;
 import org.schlunzis.kurtama.common.IUser;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,13 @@ import org.springframework.stereotype.Component;
 public class LobbyController {
 
     private final ILobbyService lobbyService;
+    private final SessionService sessionService;
 
     @FXML
     private ListView<IUser> userListView;
 
+    @FXML
+    private Button startGameButton;
 
     public void leaveLobby() {
         lobbyService.leaveLobby();
@@ -42,6 +47,9 @@ public class LobbyController {
                 }, () -> log.warn("No lobby found"));
             }
         });
+
+        IUser currentUser = sessionService.getCurrentUser().orElseThrow(() -> new IllegalStateException("No user found"));
+        lobbyService.getCurrentLobby().ifPresent(lobby -> startGameButton.setVisible(lobby.getOwner().equals(currentUser)));
     }
 
     @FXML
